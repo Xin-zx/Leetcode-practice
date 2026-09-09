6bd383f222090c4de077421178556b5bcf0b2345
Xin-zx <764668948@qq.com>
2026-09-09 22:21:36 +0800

    LC142环形链表 快慢指针解法

---
+++ src/LC142_CycleList2.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow){
+                ListNode tmp = slow;
+                fast = head;
+                while (fast != tmp){
+                    fast = fast.next;
+                    tmp = tmp.next;
+                }
+                return tmp;
+            }
+        }
+        return null;
+    }
+}
